471d34e970460cc0d7e6eb98a24f0ece218fb9da
alleyful <alleyful@naver.com>
2019-12-20 01:36:52 +0900

    useHook

--- account-book-js/src/App.js
+++ account-book-js/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState, useMemo, useCallback } from "react";
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 import AccountWrap from "./components/AccountWrap";
@@ -24,6 +24,56 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 function App() {
+  const [inputs, setInputs] = useState({
+    title: "",
+    money: 0
+  });
+
+  const { title, money } = inputs;
+  const onChange = useCallback(
+    e => {
+      const { name, value } = e.target;
+      setInputs({
+        ...inputs,
+        [name]: value
+      });
+    },
+    [inputs]
+  );
+  const [list, setList] = useState([
+    {
+      id: 1,
+      title: "식비",
+      money: -5000
+    },
+    {
+      id: 2,
+      title: "교통비",
+      money: -2000
+    },
+    {
+      id: 3,
+      title: "용돈",
+      money: 10000
+    }
+  ]);
+
+  const nextId = useRef(4);
+  const onCreate = useCallback(() => {
+    const account = {
+      id: nextId.current,
+      title,
+      money
+    };
+    setList(list.concat(account));
+
+    setInputs({
+      title: "",
+      money: 0
+    });
+    nextId.current += 1;
+  }, [list, title, money]);
+
   return (
     <>
       <GlobalStyle />
